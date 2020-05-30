1d2e9145d555ef105afd797e9891e677ff782f20
waldganger <antony.merle@yahoo.fr>
2020-05-30 15:48:28 +0200

    transfert des responsabilités de main.rs vers lib.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
@@ -14,23 +15,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Problem opening file");
-    println!("With text :\n{}", contents);
-}
+    // run(config);
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Trop peu d'arguments.")
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename})
+    if let Err(e) = minigrep::run(config) {
+        println!("Erreur dans l'application : {}", e);
+        process::exit(1);
     }
+
 }
