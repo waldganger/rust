8028370094d2607b3be885fb18d7256d07d449ee
waldganger <antony.merle@yahoo.fr>
2020-06-01 10:21:11 +0200

    minigrep avec variable d'environnement

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,16 +6,10 @@ use minigrep::Config;
 fn main() {
     let args:Vec<String> = env::args().collect();
 
-    // let config = Config::new(&args);
     let config = Config::new(&args).unwrap_or_else(|erreur| {
         println!("Erreur : {}", erreur);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    // run(config);
 
     if let Err(e) = minigrep::run(config) {
         println!("Erreur dans l'application : {}", e);
