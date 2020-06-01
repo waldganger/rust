335b82dad18147c5a853dc46ed7d2bff9b5d2a77
waldganger <antony.merle@yahoo.fr>
2020-06-01 12:03:02 +0200

    errors redirected on stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,12 +7,14 @@ fn main() {
     let args:Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|erreur| {
-        println!("Erreur : {}", erreur);
+        eprintln!("Erreur : {}", erreur);
+        
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Erreur dans l'application : {}", e);
+        eprintln!("Erreur dans l'application : {}", e);
+
         process::exit(1);
     }
 
